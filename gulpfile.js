8a76690347862a25c6c920dcd635aeacdf1b9c92
weezart <weezart@yandex.ru>
2020-12-05 21:25:40 +0300

    Добавлен подвал.

--- gulpfile.js
+++ gulpfile.js
@@ -34,6 +34,49 @@ gulp.task('css', function () {
         }))
         .pipe(gulp.dest('./css'))
 });
+
+gulp.task('bitmap', function () {
+    return gulp.src('img/**/*.{png, jpg, jpeg, gif}')
+        .pipe(imagemin([
+            imagemin.optipng({optimizationLevel: 3}),
+            imagemin.jpegtran({progressive: true}),
+        ]))
+        .pipe(gulp.dest('img'))
+});
+
+gulp.task('svg', function () {
+    return gulp.src(['img/**/*.svg', '!img/sprite.svg'])
+        .pipe(svgmin())
+        .pipe(gulp.dest('img'));
+});
+
+gulp.task('sprite', function () {
+    return gulp.src('icons/*.svg')
+        .pipe(svgmin({
+            js2svg: {
+                pretty: true
+            }
+        }))
+        .pipe(cheerio({
+            run: function ($) {
+                $('[fill]').removeAttr('fill');
+                $('[stroke]').removeAttr('stroke');
+                $('[opacity]').removeAttr('opacity');
+                $('[style]').removeAttr('style');
+            },
+            parserOptions: {xmlMode: true}
+        }))
+        .pipe(replace('&gt;', '>'))
+        .pipe(svgSprite({
+            mode: {
+                symbol: {
+                    sprite: '../sprite.svg'
+                }
+            }
+        }))
+        .pipe(gulp.dest('img'));
+});
+
 gulp.task('scripts', function () {
     return gulp.src(['./js/**/*.js', '!js/**/*.min.js'])
         .pipe(plumber())
@@ -44,4 +87,10 @@ gulp.task('scripts', function () {
         .pipe(gulp.dest('./js'))
 });
 
+gulp.task('watch', function () {
+    gulp.watch('./scss/**/*.{scss,sass}', ['styles']);;
+});
+
+gulp.task('images', ['bitmap', 'svg', 'sprite']);
+gulp.task('dev', ['styles', 'scripts', 'watch']);
 gulp.task('build', ['css', 'scripts']);
